45d21d1f025cd8e8ec6947a80c24bed1925c9732
jeremydmarx <jeremydmarx@gmail.com>
2019-12-28 02:12:01 -0600

    completed part 4 of ravenous minus extra credit prompts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,10 @@ import logo from '/Users/jeremydmarx/Codecademy/ravenous/src/logo.svg';
 import './App.css';
 import BusinessList from '/Users/jeremydmarx/Codecademy/ravenous/src/components/BusinessList/BusinessList.js';
 import SearchBar from '/Users/jeremydmarx/Codecademy/ravenous/src/components/SearchBar/SearchBar.js';
+import Yelp from '/Users/jeremydmarx/Codecademy/ravenous/src/util/Yelp.js';
 
+/*
+HARDCODED Business info
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -17,10 +20,24 @@ const business = {
 };
 
 const businesses = [business, business, business, business, business, business];
+*/
 
 class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    businesses: []
+  };
+  this.searchYelp = this.searchYelp.bind(this);
+}
+
  searchYelp(term, location, sortBy){
-   console.log(`Search Yelp with ${term}, ${location}, ${sortBy}.`);
+  //  console.log(`Search Yelp with ${term}, ${location}, ${sortBy}.`);
+  Yelp.search(term, location, sortBy).then(businesses => {
+    this.setState({
+      businesses: businesses
+    });
+  });
  }
 
   render(){
@@ -28,7 +45,7 @@ class App extends React.Component {
     <div className="App">
     <h1>ravenous</h1>
     <SearchBar searchYelp={this.searchYelp} />
-    <BusinessList businesses={businesses}/> 
+    <BusinessList businesses={this.state.businesses}/> 
   </div>
     );
    }

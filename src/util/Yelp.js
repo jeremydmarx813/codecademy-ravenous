f4adae184315f81e38130f95c47317304c9c2e0a
jeremydmarx <jeremydmarx@gmail.com>
2019-12-28 01:47:17 -0600

    finished writing yelp script file

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+import Business from "../components/Business/Business";
+
+const apiKey = 'i7sAVSE_E4fL6noCpFKsKGhhWOf_ZlLrfbDhThioVvqxcCZh6MxtPy4rUFj7NDX5y-bDiUhMzIY0Dux2n-FSHkTorEiHJTzJeQJ0zWo0JdPJrDQAdqA_bFHUt_cGXnYx';
+const Yelp = {
+    search(term, location, sortBy){
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+          headers: {
+            Authorization: `Bearer ${apiKey}`  
+          }
+      }).then(response => {
+          return response.json();
+      }).then(jsonResponse => {
+          if(jsonResponse.businesses){
+            return jsonResponse.businesses.map(business => {
+                return {
+                   id: business.id,
+                   imageSrc: business.image_url,
+                   name: business.name,
+                   address: business.location.address1,
+                   city: business.location.city,
+                   state: business.location.state,
+                   zipCode: business.location.zip_code,
+                   category: business.categories[0].title,
+                   rating: business.rating,
+                   reviewCount: business.review_count
+                 };
+            });
+          }
+      });
+    }
+};
+
+export default Yelp;
